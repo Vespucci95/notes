901a6741baf0b796ee4ffb0e096cdd05a5abdb09
Vespucci95 <donglonk@naver.com>
2025-05-01 18:54:33 +0900

    feat: 게시물 데이터를 카테고리로 가공 처리

--- src/components/snb.tsx
+++ src/components/snb.tsx
@@ -1,37 +1,95 @@
 import * as React from "react"
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import { AllSnbDataQuery } from '@/__generated__/gatsby-types'
 import { isType } from '@/types/guards';
+import { Accordion } from 'radix-ui';
+import { entries, groupBy, head, map, pipe, split, toArray } from '@fxts/core';
 
-const UNTITLED = 'untitled'
+const UNTITLED = '제목 없음'
 const NO_NAME = 'noname'
 const FILE_NAME = 'File'
+const DEFAULT_CATEGORY_NAME = '기본'
 
-const useSNB = () => {
-  const data: AllSnbDataQuery = useStaticQuery(graphql`
-      query AllSnbData {
-          allMarkdownRemark {
-              nodes {
-                  frontmatter {
-                      title
-                  }
-                  parent {
-                      __typename
-                      ... on File {
-                          relativePath
-                          name
-                      }
-                  }
-              }
-          }
-      }
-  `)
+const useSNBQuery = (): AllSnbDataQuery => useStaticQuery(graphql`
+    query AllSnbData {
+        allMarkdownRemark {
+            nodes {
+                frontmatter {
+                    title
+                }
+                parent {
+                    __typename
+                    ... on File {
+                        relativePath
+                        name
+                    }
+                }
+            }
+        }
+    }
+`)
 
+
+type SnbNode = {
+  title: string
+  path: string
+  fileName: string
+}
+
+type CategoryItem = {
+  title: string;
+  path: string;
+}
+
+type Category = {
+  categoryName: string;
+  items: CategoryItem[];
+}
+
+const convertToSnbNodes = (data: AllSnbDataQuery): SnbNode[] => {
   return data.allMarkdownRemark.nodes.map((node, index) => ({
-    title: node.frontmatter?.title ?? UNTITLED,
+    title: node.frontmatter?.title ? node.frontmatter?.title : UNTITLED,
     path: isType(node.parent, FILE_NAME) ? node.parent.relativePath : '/',
     fileName: isType(node.parent, FILE_NAME) ? node.parent.name : NO_NAME + index,
   }))
+}
+
+const isMarkDown = (fileName: string): boolean => /\.[md|mdx]+/g.test(fileName)
+
+const convertToCategory = (data: SnbNode[]): Category[] => {
+  return pipe(
+    data,
+    map(node => {
+      const categoryName = pipe(node.path, split('/'), head) ?? DEFAULT_CATEGORY_NAME;
+      return {
+        categoryName: isMarkDown(categoryName) ? DEFAULT_CATEGORY_NAME : categoryName,
+        ...node
+      }
+    }),
+    groupBy(a => a.categoryName),
+    entries,
+    map(([categoryName, nodes]) => ({
+      categoryName,
+      items: pipe(
+        nodes,
+        map(node => ({
+          title: node.title,
+          path: node.path
+        })),
+        toArray
+      )
+    })),
+    toArray
+  )
+}
+
+const useSNB = () => {
+  const data = useSNBQuery();
+  return pipe(
+    convertToSnbNodes(data),
+    convertToCategory,
+    toArray
+  )
 }
 
 const SNB = () => {
@@ -40,6 +98,24 @@ const SNB = () => {
 
   return (
     <aside>
+      <Accordion.Root
+        type='multiple'
+      >
+        {
+          snbData.map((category) => (
+            <Accordion.Item value={category.categoryName}>
+              <Accordion.Trigger>{category.categoryName}</Accordion.Trigger>
+              <Accordion.Content>
+                {
+                  category.items.map((item) => (
+                    <Link style={{ display: 'block' }} to={item.path}>{item.title}</Link>
+                  ))
+                }
+              </Accordion.Content>
+            </Accordion.Item>
+          ))
+        }
+      </Accordion.Root>
     </aside>
   )
 }
