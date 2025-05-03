f341df5033ff45176846831edb16f0e4766f6f4e
Vespucci95 <donglonk@naver.com>
2025-05-03 20:10:38 +0900

    refactor: 기본값 추가

--- src/components/post-header/post-header.tsx
+++ src/components/post-header/post-header.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import styles from './post-header.module.scss'
+import { useAppConfig } from '@/queries/app-config';
 
 type PostHeaderProps = {
   title: string;
@@ -9,13 +10,15 @@ type PostHeaderProps = {
 }
 
 const PostHeader = ({ title, category, date }: PostHeaderProps) => {
+  const { site } = useAppConfig()
+
   return (
     <motion.div
       className={styles.headerContainer}
     >
-      <h1>{title}</h1>
+      <h1>{title || site.siteMetadata.defaultPostTitle}</h1>
       <div>
-        <p>{category}</p>
+        <p>{category || site.siteMetadata.defaultCategoryName}</p>
         <p>{date}</p>
       </div>
     </motion.div>
